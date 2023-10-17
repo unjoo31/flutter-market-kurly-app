4c42873c7f3e179981244b3160cb2f31cac95d4c
unjoo31 <unjoo31@gmail.com>
2023-10-17 22:16:04 +0900

    git commit "홈 화면 > 알뜰쇼핑 화면 완료"

--- lib/screens/home/components/thrifty_shopping_page.dart
+++ lib/screens/home/components/thrifty_shopping_page.dart
@@ -1,10 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_market_kurly_app/screens/home/components/product_filter_button.dart';
+
+import '../../../models/product.dart';
+import '../../components/circle_container.dart';
+import '../../components/product_item.dart';
 
 class ThrifyShoppingPage extends StatelessWidget {
   const ThrifyShoppingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              SizedBox(
+                height: 120,
+                child: Image.asset(
+                  "assets/images/kurly_banner_1.jpg",
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: SizedBox(
+                  child: ProductFilterButton(),
+                  width: 100,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SliverPadding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          sliver: SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              mainAxisSpacing: 30,
+              crossAxisSpacing: 8,
+              childAspectRatio: 0.6,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Stack(
+                  children: [
+                    ProductItem(
+                      product: productList[index],
+                    ),
+                    Positioned(
+                      bottom: 90,
+                      right: 10,
+                      child: CircleContainer(
+                          iconPath: 'assets/icons/shopping-cart.svg'),
+                    )
+                  ],
+                );
+              },
+              childCount: productList.length,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
