0d47f4f623b7b28a9e926544426b3a885bf7312a
unjoo31 <unjoo31@gmail.com>
2023-10-17 20:56:14 +0900

    git commit "홈 화면 appbar 완료"

---
+++ lib/screens/home/components/kurly_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class KurlyPage extends StatelessWidget {
+  const KurlyPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
